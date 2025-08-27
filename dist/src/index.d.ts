12171b78ef18d861e126649e65fa05ac927d6121
Martin Hamel <martin@ma4s.org>
2025-08-27 19:30:53 +0000

    index

---
+++ dist/src/index.d.ts
@@ -0,0 +1,2 @@
+export * from './types/user';
+export * from './types/user_info';
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// Re-export commonly used types from this package to provide a single entry point.
+export * from './types/user';
+export * from './types/user_info';
+// Add other re-exports as needed
